21be83d443bf98329373d55a371b758bbdf7b7b6
Owyll <wyll82160@gmail.com>
2026-04-01 10:45:55 -0300

    Add files via upload

---
+++ #Verificador de login.py
@@ -0,0 +1,7 @@
+#Verificador de login
+usuario = input("Digite o Usuario")
+if usuario == "admin":
+    print("Acesso liberado")
+else:
+    print("Acesso Negado")
+#--wylamys junio marcolino dos santos
