60abd926112ba7ff3dd5e793f929a14b985598b7
Owyll <wyll82160@gmail.com>
2026-04-01 11:19:48 -0300

    Add files via upload

---
+++ lanche.py
@@ -0,0 +1,8 @@
+valor = float(input("Digite o Valor do lanche: R$"))
+if valor <= 10.00:
+    print("Preço Justo")
+elif 10.01 <= valor <= 20.00:
+    print("Está ficando Caro")
+else:
+    print("Está muito caro, melhor levar de casa!")
+#--Wylamys junio Marcolino dos santos
