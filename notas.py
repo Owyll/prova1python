2e033f8ccbe56c08b7d35474be0a75627c261cdc
Owyll <wyll82160@gmail.com>
2026-04-01 11:32:11 -0300

    Add files via upload

---
+++ notas.py
@@ -0,0 +1,7 @@
+nota1 = float(input("Digite a Primeira Nota"))
+nota2 = float(input("Digite a Segunda Nota"))
+nota3 = float(input("Digite a Terceira nota"))
+
+media = (nota1 + nota2 + nota3) / 3
+print(f"media: {media:.2f}")
+#--wylamys junio marcolino dos santos
